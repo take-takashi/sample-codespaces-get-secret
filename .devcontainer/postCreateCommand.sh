1f4d249e4a820b9abb4923b043d8aab4bcf2a44f
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-27 18:55:59 +0900

    Create postCreateCommand.sh

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# WORKSPACE=$PWD
+
+# npm install for cdk
+# cd ${WORKSPACE}/cdk
+# npm ci
+
+# npm install for app
+# cd ${WORKSPACE}/app
+# npm ci
+
+# create .env file
+# cd ${WORKSPACE}/app
+# echo '' > .env
+## .env for prisma
+# echo DATABASE_URL=${DEV_DATABASE_URL} >> .env
+
+# create .env.production file
+# cd ${WORKSPACE}/app
+# echo '' > .env.production
+## .env for prisma
+# echo DATABASE_URL=${PRD_DATABASE_URL} >> .env.production
